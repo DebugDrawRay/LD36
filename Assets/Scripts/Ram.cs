b39487af347c16ab9d9f49bc3a558e436d93f07f
Raymond John Grice <raymondjgrice@gmail.com>
2016-08-27 12:29:49 -0700

    Added raming functionality

---
+++ Assets/Scripts/Ram.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ram : Action
+{
+    public InputState.Actions assignedAction;
+    public float strength;
+    public float damage;
+
+    private Rigidbody rigid;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        rigid = GetComponent<Rigidbody>();
+    }
+    protected override void Execute(InputState actions)
+    {
+        if(actions.CheckAction(assignedAction))
+        {
+            Vector3 dir = actions.target.position - transform.position;
+            dir.y = 0;
+            rigid.AddForce(dir * strength, ForceMode.Impulse);
+        }
+    }
+}
